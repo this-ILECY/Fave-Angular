068152fa87bd3c13bd88ff8ed2ae5b04aad85d4a
Mardani <Mardani.Amirhosein@WiraPos.com>
2021-12-29 10:52:23 +0330

    shopPromotionAdded

---
+++ tenetShop/shopPromotion/script/script.js
@@ -0,0 +1,32 @@
+// function initMap() {
+//     // The location of Uluru
+//     const uluru = { lat: -25.344, lng: 131.036 };
+//     // The map, centered at Uluru
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         center: uluru,
+//     });
+//     // The marker, positioned at Uluru
+//     const marker = new google.maps.Marker({
+//         position: uluru,
+//         map: map,
+//     });
+// }
+
+// var myMap = new ol.Map({
+//     target: "tenet-neshan-map",
+//     key:  "web.NexSTlHWvRoUaozyhTfJsufSxMwfI2c2SVYACL8f",
+//     view: new ol.View({
+//     center: ol.proj.fromLonLat([51.338076,35.699756]),
+//     zoom: 17 })
+// })
+// // myMap.setMapType("standard-night");
+// myMap.setMapType("standard-day");
+//     // var myMap = new L.Map('tenet-neshan-map', {
+//     //     key: 'web.NexSTlHWvRoUaozyhTfJsufSxMwfI2c2SVYACL8f',
+//     //     maptype: 'dreamy',
+//     //     poi: true,
+//     //     traffic: false,
+//     //     center: [35.699739, 51.338097],
+//     //     zoom: 14
+//     // });
